80e8b5f22f3c0b6c47eac10a2b6dfc1b19f57777
Odunlami <Alani.Odunlami@ons.gov.uk>
2026-08-28 14:46:50 +0100

    feat: add examples test data

--- fdr_toolkit/fdr.py
+++ fdr_toolkit/fdr.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+"""
+The fdr for the three methods to consider in this work
+"""
+
 def bonferroni(p_values, alpha=0.05):
     p_values = np.array(p_values)
     m = len(p_values)
